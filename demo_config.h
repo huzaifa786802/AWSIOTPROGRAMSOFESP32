a5260928e9110c7d0fd5561eb2e4023410ccbb03
Huzaifa Shahbaz <127684198+huzaifa786802@users.noreply.github.com>
2024-09-27 14:31:40 +0500

    Add files via upload

---
+++ demo_config.h
@@ -0,0 +1,117 @@
+/*
+ * demo_config.h
+ *
+ *  Created on: Jul 12, 2024
+ *      Author: HUZAIFASHAHBAZ
+ */
+#ifndef DEMO_CONFIG_H_
+#define DEMO_CONFIG_H_
+/**************************************************/
+/******* DO NOT CHANGE the following order ********/
+/**************************************************/
+/* Logging related header files are required to be included in the following order:
+ * 1. Include the header file "logging_levels.h".
+ * 2. Define LIBRARY_LOG_NAME and  LIBRARY_LOG_LEVEL.
+ * 3. Include the header file "logging_stack.h".
+ */
+/* Include header that defines log levels. */
+#include "logging_levels.h"
+/* Logging configuration for the Demo. */
+#ifndef LIBRARY_LOG_NAME
+    #define LIBRARY_LOG_NAME     "MQTT_DEMO"
+#endif
+#ifndef LIBRARY_LOG_LEVEL
+    #define LIBRARY_LOG_LEVEL    LOG_INFO
+#endif
+#include "logging_stack.h"
+/************ End of logging configuration ****************/
+/**
+ * @brief Details of the MQTT broker to connect to.
+ *
+ * @note Your AWS IoT Core endpoint can be found in the AWS IoT console under
+ * Settings/Custom Endpoint, or using the describe-endpoint API.
+ *
+ */
+#ifndef AWS_IOT_ENDPOINT
+    #define AWS_IOT_ENDPOINT    CONFIG_MQTT_BROKER_ENDPOINT
+#endif
+/**
+ * @brief AWS IoT MQTT broker port number.
+ *
+ * In general, port 8883 is for secured MQTT connections.
+ *
+ * @note Port 443 requires use of the ALPN TLS extension with the ALPN protocol
+ * name. When using port 8883, ALPN is not required.
+ */
+#ifndef AWS_MQTT_PORT
+    #define AWS_MQTT_PORT    ( CONFIG_MQTT_BROKER_PORT )
+#endif
+/**
+ * @brief The username value for authenticating client to MQTT broker when
+ * username/password based client authentication is used.
+ *
+ * Refer to the AWS IoT documentation below for details regarding client
+ * authentication with a username and password.
+ * https://docs.aws.amazon.com/iot/latest/developerguide/custom-authentication.html
+ * As mentioned in the link above, an authorizer setup needs to be done to use
+ * username/password based client authentication.
+ *
+ * @note AWS IoT message broker requires either a set of client certificate/private key
+ * or username/password to authenticate the client. If this config is defined,
+ * the username and password will be used instead of the client certificate and
+ * private key for client authentication.
+ *
+ * #define CLIENT_USERNAME    "...insert here..."
+ */
+/**
+ * @brief The password value for authenticating client to MQTT broker when
+ * username/password based client authentication is used.
+ *
+ * Refer to the AWS IoT documentation below for details regarding client
+ * authentication with a username and password.
+ * https://docs.aws.amazon.com/iot/latest/developerguide/custom-authentication.html
+ * As mentioned in the link above, an authorizer setup needs to be done to use
+ * username/password based client authentication.
+ *
+ * @note AWS IoT message broker requires either a set of client certificate/private key
+ * or username/password to authenticate the client.
+ *
+ * #define CLIENT_PASSWORD    "...insert here..."
+ */
+/**
+ * @brief MQTT client identifier.
+ *
+ * No two clients may use the same client identifier simultaneously.
+ */
+#ifndef CLIENT_IDENTIFIER
+    #define CLIENT_IDENTIFIER    CONFIG_MQTT_CLIENT_IDENTIFIER
+#endif
+/**
+ * @brief Size of the network buffer for MQTT packets.
+ */
+#define NETWORK_BUFFER_SIZE       ( CONFIG_MQTT_NETWORK_BUFFER_SIZE )
+/**
+ * @brief The name of the operating system that the application is running on.
+ * The current value is given as an example. Please update for your specific
+ * operating system.
+ */
+#define OS_NAME                   "FreeRTOS"
+/**
+ * @brief The version of the operating system that the application is running
+ * on. The current value is given as an example. Please update for your specific
+ * operating system version.
+ */
+#define OS_VERSION                tskKERNEL_VERSION_NUMBER
+/**
+ * @brief The name of the hardware platform the application is running on. The
+ * current value is given as an example. Please update for your specific
+ * hardware platform.
+ */
+#define HARDWARE_PLATFORM_NAME    CONFIG_HARDWARE_PLATFORM_NAME
+/**
+ * @brief The name of the MQTT library used and its version, following an "@"
+ * symbol.
+ */
+#include "core_mqtt.h"
+#define MQTT_LIB    "core-mqtt@" MQTT_LIBRARY_VERSION
+#endif /* ifndef DEMO_CONFIG_H_ */
